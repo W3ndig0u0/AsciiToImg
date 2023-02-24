0223c9ce63688894f45c28b5dc49dd2e9ae3e670
W3ndig0 <Jingxiang304@gmail.com>
2023-02-24 16:17:09 +0100

    somethign uesful💀

---
+++ loader.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Drawing;
+
+namespace asciiToImg
+{
+    class Loader
+    {
+        public Loader()
+        {
+            //'NOTE: load Img
+            Bitmap img = new Bitmap("bo2.jpg");
+
+        }
+    }
+}
